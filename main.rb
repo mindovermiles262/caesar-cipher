ce9dfae80716dc1b8a59010de41cc42d71b41d8d
Erik Johnson <mindovermiles262@gmail.com>
2017-04-30 20:59:27 -0700

    change default form inputs

--- main.rb
+++ main.rb
@@ -6,8 +6,8 @@ require './lib/caesar'
 
 get '/' do
 	key = params['key'].to_i || 5
-	message = params['message'] || "What a string!"
+	message = params['message'] || 'Enter Your Message'
 	processed = CaesarCipher.encrypt(message, key)
-	erb :index, :locals => { :processed => processed, 
-							:message => message }
+	erb :index, :locals => { 	:processed => processed, 
+								:message => message }
 end
