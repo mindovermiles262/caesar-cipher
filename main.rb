fb3541ea7dac3d499e91edc7ef4d55797063b69d
Erik Johnson <mindovermiles262@gmail.com>
2017-05-02 08:53:04 -0700

    css style, mobile views

--- main.rb
+++ main.rb
@@ -5,9 +5,10 @@ require './lib/caesar'
 
 
 get '/' do
-	key = params['key'].to_i || 5
+	key = params['key'] || 5
 	message = params['message'] || 'Enter Your Message'
-	processed = CaesarCipher.encrypt(message, key)
+	processed = CaesarCipher.encrypt(message, key.to_i)
+
 	erb :index, :locals => { 	:processed => processed, 
 								:message => message }
 end
